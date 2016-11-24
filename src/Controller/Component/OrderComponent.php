c87b835e2e54ad34a3ff5a24d82cf480c15c5739
Nguyen Xuan Bach <bachnx@hiworld.com.vn>
2016-11-24 12:26:33 +0700

    first commit

---
+++ src/Controller/Component/OrderComponent.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nguyenxuanbach
+ * Date: 11/23/2016
+ * Time: 4:24 PM
+ */
+
+namespace App\Controller\Component;
+
+use Cake\Controller\Component;
+
+class OrderComponent extends Component
+{
+    public function orderSlide(){
+
+    }
+}
